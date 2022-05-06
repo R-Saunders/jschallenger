587d480cdeb954d8ab9c01714c088cd8a428e8f8
Rich Saunders <richsaunders@protonmail.com>
2022-05-06 21:59:53 +0100

    First five challenges completed

---
+++ Javascript Basic.js
@@ -0,0 +1,28 @@
+// 1.
+
+function myFunction(a, b) {
+    return a+b;
+}
+
+// 2.
+
+function myFunction(a, b){
+    return a === b;
+}
+
+// 3.
+
+function myFunction(a){
+    return typeof a;
+}
+
+// 4.
+
+function myFunction(a,n) {
+    return a[n-1];
+}
+
+// 5.
+function myFunction(a) {
+    return a.slice(3);
+ }
