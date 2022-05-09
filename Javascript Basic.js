dc7fd685fac000b5dbac618aa29b0d0e4572c394
Rich Saunders <richsaunders@protonmail.com>
2022-05-09 23:00:37 +0100

    Completed up to 9.

--- Javascript Basic.js
+++ Javascript Basic.js
@@ -32,3 +32,20 @@ function myFunction(str) {
     return str.substr(-3, 3);
 }
 
+// 7
+function myFunction(a) {
+    return a.substr(0,3);
+}
+
+// 8
+function myFunction(a) {
+    return a.slice(0, a.length/2);
+}
+
+// 9
+
+function myFunction(a) {
+    return a.slice(0,-3);
+ }
+
+ 
